c59d7eeb30563f50a099e30c06646761f3f0ca1c
saitejachalla21 <challasaiteja21@gmail.com>
2025-04-27 22:57:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Image Encryption App", layout="centered")
+
+st.title("🔐 Image Encryption & Decryption")
+
+st.markdown("Use the sidebar to navigate between encryption, decryption, comparison, benchmarks, Analysis.")
